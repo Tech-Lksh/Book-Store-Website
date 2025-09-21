2911c96616a26d77ccee194d22e67e1ed5301c21
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-09-21 22:58:24 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+   import React from "react";
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
